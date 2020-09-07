c62a67af6ac7c13d4ba797665a982d1d7ea0f51d
kkingkangg <phungnghia152@gmail.com>
2020-09-07 16:43:34 +0700

    done

--- Cau1/Cau1.js
+++ Cau1/Cau1.js
@@ -1,9 +1,10 @@
 
 function equation (a,b) {
     if (a == 0) {
-        document.write('Phương trình không phải phương trình bậc nhất');
-    }
-    else {
+        if(b==0)
+            document.write('Phương trình vô số nghiệm');
+         else
+            document.write('Phương trình vô nghiệm');
+    } else
         document.write('Nghiệm của phương trình là: ' + (-b/a));
-    }
 }
