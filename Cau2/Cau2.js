2c45e66532323afc1652e13193cb4baa85a7ac8b
kkingkangg <phungnghia152@gmail.com>
2020-09-07 16:33:28 +0700

    done

--- Cau2/Cau2.js
+++ Cau2/Cau2.js
@@ -1,6 +1,8 @@
+let count;
 function checkNumber(number, array) {
     for (let i = 0; i < array.length; i++) {
         if(number == array[i]) {
+            count = i;
             return true;
         }
     }
